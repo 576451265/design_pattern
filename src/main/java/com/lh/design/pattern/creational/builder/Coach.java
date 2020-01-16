18cbb40ed67eb1bb841fe25c16a5aa78c69f63df
576451265 <576451265@qq.com>
2020-01-16 18:06:23 +0800

    建造者模式

---
+++ src/main/java/com/lh/design/pattern/creational/builder/Coach.java
@@ -0,0 +1,23 @@
+package com.lh.design.pattern.creational.builder;
+
+/**
+ * @Description:
+ * @author LuoH
+ * @date 2020/01/16
+ */
+public class Coach {
+    private CourseBuilder courseBuilder;
+
+    public void setCourseBuilder(CourseBuilder courseBuilder) {
+        this.courseBuilder = courseBuilder;
+    }
+
+    public Course makeCourse(String courseName,String coursePPT,String courseVideo,String courseArticle,String courseQA){
+        this.courseBuilder.buildCourseName(courseName);
+        this.courseBuilder.buildCoursePPT(coursePPT);
+        this.courseBuilder.buildCourseVideo(courseVideo);
+        this.courseBuilder.buildCourseArticle(courseArticle);
+        this.courseBuilder.buildCourseQA(courseQA);
+        return this.courseBuilder.makeCourse();
+    }
+}
---
+++ src/main/java/com/lh/design/pattern/creational/builder/v2/Course.java
@@ -0,0 +1,73 @@
+package com.lh.design.pattern.creational.builder.v2;
+
+/**
+ * @Description: 静态内部类的方法
+ * @author LuoH
+ * @date 2020/01/16
+ */
+public class Course {
+    private String courseName;
+    private String coursePPT;
+    private String courseVideo;
+    private String courseArticle;
+    //question & answer
+    private String courseQA;
+
+    public Course(CourseBuilder courseBuilder) {
+        this.courseName = courseBuilder.courseName;
+        this.coursePPT = courseBuilder.coursePPT;
+        this.courseVideo = courseBuilder.courseVideo;
+        this.courseArticle = courseBuilder.courseArticle;
+        this.courseQA = courseBuilder.courseQA;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseName='" + courseName + '\'' +
+                ", coursePPT='" + coursePPT + '\'' +
+                ", courseVideo='" + courseVideo + '\'' +
+                ", courseArticle='" + courseArticle + '\'' +
+                ", courseQA='" + courseQA + '\'' +
+                '}';
+    }
+
+    public static class CourseBuilder {
+        private String courseName;
+        private String coursePPT;
+        private String courseVideo;
+        private String courseArticle;
+        //question & answer
+        private String courseQA;
+
+        //链式调用
+        public CourseBuilder buildCourseName(String courseName) {
+            this.courseName = courseName;
+            return this;
+        }
+
+        public CourseBuilder buildCoursePPT(String coursePPT) {
+            this.coursePPT = coursePPT;
+            return this;
+        }
+
+        public CourseBuilder buildCourseVideo(String courseVideo) {
+            this.courseVideo = courseVideo;
+            return this;
+        }
+
+        public CourseBuilder buildCourseArticle(String courseArticle) {
+            this.courseArticle = courseArticle;
+            return this;
+        }
+
+        public CourseBuilder buildCourseQA(String courseQA) {
+            this.courseQA = courseQA;
+            return this;
+        }
+
+        public Course build() {
+            return new Course(this);
+        }
+    }
+}
